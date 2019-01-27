7c35bff379f9f03680d3fc80c962cc3504234e58
Nick <nickfarrell@gmail.com>
2019-01-27 12:04:27 -0800

    Add menu to choose sort criteria

--- app/src/main/java/com/example/popularmoviesstage1/MainActivity.java
+++ app/src/main/java/com/example/popularmoviesstage1/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.popularmoviesstage1;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.sort_by, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.sort_by_popular) {
+            Context context = MainActivity.this;
+            String textToShow = "Sorting movies by popularity...";
+            Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (itemThatWasClickedId == R.id.sort_by_rating) {
+            Context context = MainActivity.this;
+            String textToShow = "Sorting movies by rating...";
+            Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
